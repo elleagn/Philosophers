8a4c90ed004e26b1408623fec56b707121679329
Gaelle Ozon <gozon@made-f0Br8s12.clusters.42paris.fr>
2024-09-27 14:08:38 +0200

    Add char to ms conversion + vars struct

---
+++ philo/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 13:38:33 by gozon             #+#    #+#             */
+/*   Updated: 2024/09/27 14:08:02 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+typedef struct s_vars
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_meals;
+}	t_vars;
+
+#endif
