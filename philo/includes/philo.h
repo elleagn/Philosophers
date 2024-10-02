4418489c028aedf327475890d39b3ac73b156dc5
Gaelle Ozon <gozon@made-f0Br8s8.clusters.42paris.fr>
2024-10-02 08:18:55 +0200

    Start to write structures

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,12 +6,15 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:38:33 by gozon             #+#    #+#             */
-/*   Updated: 2024/09/27 14:08:02 by gozon            ###   ########.fr       */
+/*   Updated: 2024/10/01 08:19:42 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
 
 typedef struct s_vars
 {
@@ -21,5 +24,21 @@ typedef struct s_vars
 	int	time_to_sleep;
 	int	number_of_meals;
 }	t_vars;
+
+typedef struct s_philosopher
+{
+	int	id;
+	int	is_eating;
+	int	is_sleeping;
+	int	is_thinking;
+	int	is_printed;
+	int	start_of_latest_meal;
+}	t_philosopher;
+
+typedef struct s_fork
+{
+	int	id;
+	int	is_taken;
+}	t_fork;
 
 #endif
